1604e860bd737391b217401c856ac049076e1d4d
= <dominikasmorag@gmail.com>
2023-06-03 18:58:32 +0200

    embeddable position class

---
+++ src/main/java/com/dominika/springbootapp/pojo/Position.java
@@ -0,0 +1,14 @@
+package com.dominika.springbootapp.pojo;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Position {
+    private double lat;
+    private double lon;
+
+    public Position(double lat, double lon) {
+        this.lat = lat;
+        this.lon = lon;
+    }
+}
