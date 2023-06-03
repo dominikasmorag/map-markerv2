f7c178398463cf0758fd4ad25f2726d657e70b0d
= <dominikasmorag@gmail.com>
2023-06-03 19:04:57 +0200

    constructor for embeddable class position

--- src/main/java/com/dominika/springbootapp/pojo/Position.java
+++ src/main/java/com/dominika/springbootapp/pojo/Position.java
@@ -7,8 +7,13 @@ public class Position {
     private double lat;
     private double lon;
 
+    public Position() {
+    }
+
     public Position(double lat, double lon) {
         this.lat = lat;
         this.lon = lon;
     }
+
+
 }
