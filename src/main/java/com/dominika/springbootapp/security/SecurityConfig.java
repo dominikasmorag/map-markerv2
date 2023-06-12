1b38041355a72f0dd52a1af5c8da42ba71b5b856
= <dominikasmorag@gmail.com>
2023-06-12 21:38:12 +0200

    disabled csrf

--- src/main/java/com/dominika/springbootapp/security/SecurityConfig.java
+++ src/main/java/com/dominika/springbootapp/security/SecurityConfig.java
@@ -1,45 +1,56 @@
 package com.dominika.springbootapp.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-//
-//    public CustomUserDetailsService getUserDetailsService() {
-//        return userDetailsService;
-//    }
-//
-//    private CustomUserDetailsService userDetailsService;
-//
-//    @Autowired
-//    public SecurityConfig(CustomUserDetailsService userDetailsService) {
-//        this.userDetailsService = userDetailsService;
-//    }
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Bean
-    public SecurityFilterChain FilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .anyRequest().hasAnyRole("ADMIN")
-//                )
-//                .formLogin(Customizer.withDefaults());
-//        return http.build();
+    InMemoryUserDetailsManager users() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("test")
+                        .password("{noop}password")
+                        .roles("ADMIN")
+                        .build()
+        );
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        System.out.println("\n\n\n****\n\nSecurityConfig.securityFilterChain");
+        if(applicationContext == null ) {
+            System.out.println("context is null");
+        }
+        Customizer<CsrfConfigurer<HttpSecurity>> customizer = httpSecurityCsrfConfigurer -> {
+            CsrfConfigurer<HttpSecurity> configurer = new CsrfConfigurer<>(applicationContext);
+            configurer.setBuilder(http);
+            configurer.disable();
+
+        };
+        System.out.println("customizer created");
         return http
+                .csrf(customizer)
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated()
                 )
@@ -47,28 +58,15 @@ public class SecurityConfig {
                 .build();
     }
 
-    @Bean
-    public UserDetailsService users() {
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password("admin123")
-                .roles("ADMIN")
-                .build();
-        UserDetails user = User.builder()
-                .username("user")
-                .password("user123")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(admin, user);
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource).usersByUsernameQuery(
+                        "select username, password, 1 " +
+                        "from users " +
+                        "where username = ?"
+                ).authoritiesByUsernameQuery();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-         return authenticationConfiguration.getAuthenticationManager();
-        }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
