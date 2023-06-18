1d3b412b5ac702147a4c9179fb5b9456df546cdd
= <dominikasmorag@gmail.com>
2023-06-18 20:48:58 +0200

    latitude and longitude type changed to String

--- src/main/java/com/dominika/springbootapp/pojo/Position.java
+++ src/main/java/com/dominika/springbootapp/pojo/Position.java
@@ -4,13 +4,13 @@ import jakarta.persistence.Embeddable;
 
 @Embeddable
 public class Position {
-    private double lat;
-    private double lon;
+    private String lat;
+    private String lon;
 
     public Position() {
     }
 
-    public Position(double lat, double lon) {
+    public Position(String lat, String lon) {
         this.lat = lat;
         this.lon = lon;
     }
@@ -21,5 +21,21 @@ public class Position {
                 "lat=" + lat +
                 ", lon=" + lon +
                 '}';
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
     }
 }
