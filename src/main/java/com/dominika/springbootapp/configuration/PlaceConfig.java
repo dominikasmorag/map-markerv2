f08c0c78dd509ffcd946e6fd4f6b092b31b6ab06
= <dominikasmorag@gmail.com>
2023-06-18 20:55:14 +0200

    redundant imports removed

--- src/main/java/com/dominika/springbootapp/configuration/PlaceConfig.java
+++ src/main/java/com/dominika/springbootapp/configuration/PlaceConfig.java
@@ -1,23 +1,16 @@
 package com.dominika.springbootapp.configuration;
 
-import com.dominika.springbootapp.entity.Place;
 import com.dominika.springbootapp.repository.PlaceRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
 
 @Configuration
 public class PlaceConfig {
     PlaceRepository repository;
     @Bean
     CommandLineRunner placeCmdLineRunner(PlaceRepository repository) {
-        return args -> {
-            List<Place> list = List.of(
-
-            );
-        };
-
+        return args -> {};
     }
 }
