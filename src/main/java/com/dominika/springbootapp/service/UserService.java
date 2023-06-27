24e1cfed8226e6e961be9f8f4ca6fd1e349049c2
= <dominikasmorag@gmail.com>
2023-06-27 20:09:33 +0200

    method currentUser intended for return currently logged in user from context

--- src/main/java/com/dominika/springbootapp/service/UserService.java
+++ src/main/java/com/dominika/springbootapp/service/UserService.java
@@ -3,6 +3,9 @@ package com.dominika.springbootapp.service;
 import com.dominika.springbootapp.entity.User;
 import com.dominika.springbootapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,5 +21,14 @@ public class UserService {
 
     public List<User> getUsers() {
         return repository.findAll();
+    }
+
+    public User currentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            return null;
+        }
+        String username = ((org.springframework.security.core.userdetails.User) authentication.getPrincipal()).getUsername();
+        return repository.findByUsername(username);
     }
 }
