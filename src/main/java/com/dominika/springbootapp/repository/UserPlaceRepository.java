6fa47e05e73fffef5fbfa97a977bb50d1a7c909e
= <dominikasmorag@gmail.com>
2023-06-28 19:13:06 +0200

    method to retrieve user's places keys

--- src/main/java/com/dominika/springbootapp/repository/UserPlaceRepository.java
+++ src/main/java/com/dominika/springbootapp/repository/UserPlaceRepository.java
@@ -3,8 +3,15 @@ package com.dominika.springbootapp.repository;
 import com.dominika.springbootapp.entity.UserPlace;
 import com.dominika.springbootapp.entity.UserPlacesKey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserPlaceRepository extends JpaRepository<UserPlace, UserPlacesKey> {
+
+    @Query("SELECT up FROM UserPlace up WHERE up.userPlacesKey.userId = :userId")
+    List<UserPlace> findUserPlaces(@Param("userId") Long userId);
 }
--- src/main/java/com/dominika/springbootapp/service/UserPlaceService.java
+++ src/main/java/com/dominika/springbootapp/service/UserPlaceService.java
@@ -7,6 +7,8 @@ import com.dominika.springbootapp.repository.UserPlaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserPlaceService {
     UserPlaceRepository repository;
@@ -16,6 +18,7 @@ public class UserPlaceService {
         this.repository = repository;
     }
 
+    public List<UserPlace> findUserPlaces(Long userId) { return repository.findUserPlaces(userId); }
     public void saveUserPlace(User user, Place place) {
         repository.save(new UserPlace(user, place));
     }
