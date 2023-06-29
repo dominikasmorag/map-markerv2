aed4fc7dfc4b265b7014071a0ea578f1527c0432
= <dominikasmorag@gmail.com>
2023-06-29 20:33:21 +0200

    findSharedPlaces method intended for retrieve all shared places from each user

--- src/main/java/com/dominika/springbootapp/repository/PlaceRepository.java
+++ src/main/java/com/dominika/springbootapp/repository/PlaceRepository.java
@@ -2,9 +2,14 @@ package com.dominika.springbootapp.repository;
 
 import com.dominika.springbootapp.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PlaceRepository extends JpaRepository<Place, Long> {
 
+    @Query("SELECT p FROM Place p WHERE p.shared=true")
+    List<Place> findSharedPlaces();
 }
