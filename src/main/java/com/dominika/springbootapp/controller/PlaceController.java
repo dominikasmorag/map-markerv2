69fde9c49aec07a8363affa06833383c0b3e56b3
= <dominikasmorag@gmail.com>
2023-07-05 19:51:01 +0200

    added some endpoints to get a number of user's places and shared places in order to set the size of array in js

--- src/main/java/com/dominika/springbootapp/controller/PlaceController.java
+++ src/main/java/com/dominika/springbootapp/controller/PlaceController.java
@@ -3,6 +3,7 @@ package com.dominika.springbootapp.controller;
 import com.dominika.springbootapp.entity.Place;
 import com.dominika.springbootapp.service.PlaceService;
 import com.dominika.springbootapp.service.UserPlaceService;
+import com.dominika.springbootapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,25 +12,21 @@ import java.util.List;
 @RestController
 @RequestMapping(path="/api/v1")
 public class PlaceController {
-    private final PlaceService placeService;
-    private final UserPlaceService userPlaceService;
+    private PlaceService placeService;
+    private UserPlaceService userPlaceService;
+    private UserService userService;
 
     @Autowired
-    public PlaceController(PlaceService placeService, UserPlaceService userPlaceService) {
+    public PlaceController(PlaceService placeService, UserPlaceService userPlaceService,  UserService userService) {
         this.placeService = placeService;
         this.userPlaceService = userPlaceService;
+        this.userService = userService;
     }
 
-    @GetMapping("/places")
-    public List<Place> getPlaces() {
-        return placeService.getPlaces();
-    }
-
-    @GetMapping("/places/{id}")
+    @GetMapping("/places/myPlaces")
     @ResponseBody
-    public List<Place> getUserPlaces(@PathVariable(value="id") Long userId) { return userPlaceService.findUserPlaces(userId);}
+    public List<Place> getUserPlaces() { return userPlaceService.findUserPlaces(userService.currentUserId());}
 
-    //this method is not finished yet in javascript
     @GetMapping("/places/shared")
     @ResponseBody
     public List<Place> getSharedPlaces() { return placeService.findSharedPlaces();
@@ -37,6 +34,17 @@ public class PlaceController {
     @PostMapping("/savePlace")
     public void saveLocation(@RequestBody Place place) {
         placeService.savePlace(place);
+    }
+
+
+    @GetMapping("places/userPlacesQuantity")
+    public int countUserPlaces() {
+        return userPlaceService.countUserPlaces(userService.currentUser());
+    }
+
+    @GetMapping("places/sharedPlacesQuantity")
+    public int countSharedPlaces() {
+        return userPlaceService.countSharedPlaces();
     }
 
 }
